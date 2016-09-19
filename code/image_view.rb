cec516b854cfc7abcb1b39b8aaee9a732f661d9d
mbassmvh <markvh@manchester.ac.uk>
2016-09-19 23:19:04 +0100

    change to show fix trailing spaces ui problem

--- code/image_view.rb
+++ code/image_view.rb
@@ -12,7 +12,7 @@ class ImageView
   end
 
   def set_file_name_using_insert_str(insert_str)
-    potential_new_fn = potential_new_filename(insert_str)
+    potential_new_fn = potential_new_filename(insert_str.strip) #todo fix trailing spaces ui
     if potential_new_fn
       if @image_library.change_name(potential_new_fn)
         @insertion_text.highlightbackground = 'green'
