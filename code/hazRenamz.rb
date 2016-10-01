5d9dc7c41f2c9ebedb211f7a5f103bcde22bdbcc
mbassmvh <markvh@manchester.ac.uk>
2016-10-01 19:25:04 +0100

    changing images, no no-renderer image showing

--- code/hazRenamz.rb
+++ code/hazRenamz.rb
@@ -1,4 +1,3 @@
-
 $col_0 = '15%'
 $col_1 = '25%'
 $col_2 = '60%'
@@ -6,29 +5,28 @@ $col_2 = '60%'
 require __dir__+'/requires'
 
 Shoes.app(title: "Haz Renamz",
-           width: 800, height: 800, scroll: true) do
+          width: 800, height: 800, scroll: true) do
 
   def get_image
     @image
   end
 
   def set_image(img)
+    puts "setimage #{img}"
+
     image = @controller.image
     scale = image.scale_factor(500)
     if scale < 1.0
-      puts 'setimage reduced'
+      puts "setimage reduced "
 
-      image = MiniMagick::Image.open(img)
-      puts image.path
-      image.resize '500x500'
-      image.format 'png'
-      fn = ruby_root + '/tmp/display.png'
-      FileUtils.rm_f( fn )
-      image.write( fn )
-      @image.clear { image( fn ) }
+      im = MiniMagick::Image.open(img)
+      im.resize '500x500'
+      im.format 'jpg'
+
+      @stk.clear { image(im.path) }
     else
       puts 'setimage un-reduced'
-      @image.clear { image( img ) }
+      @stk.clear { image(img) }
     end
   end
 
@@ -42,9 +40,9 @@ Shoes.app(title: "Haz Renamz",
 
   def button_title_field(button, button_proc, field_legend, setup_edit_line_proc)
     flow do
-      stack(width: $col_0) { button(button)  { button_proc.call } }
+      stack(width: $col_0) { button(button) { button_proc.call } }
       stack(width: $col_1) { para(field_legend) }
-      stack(width: $col_2) { setup_edit_line_proc.call  }
+      stack(width: $col_2) { setup_edit_line_proc.call }
     end
   end
 
@@ -60,17 +58,18 @@ Shoes.app(title: "Haz Renamz",
   @controller.slf = self
 
 
-  three_button( 'dir',  proc { @controller.dir },
-               'prev',  proc { @controller.previous },
-               'next',  proc { @controller.next } )
+  three_button('dir', proc { @controller.dir },
+               'prev', proc { @controller.previous },
+               'next', proc { @controller.next })
 
-  button_title_field( 'test', proc { @controller.test }, 'original', proc { @original_fn = edit_line } )
+  button_title_field('test', proc { @controller.test }, 'original', proc { @original_fn = edit_line })
 
-  title_field( 'description', proc { @description = edit_line } )
+  title_field('description', proc { @description = edit_line })
 
-  title_field( 'current', proc { @current_fn = edit_line } )
-
-  append(@image = image('/Users/mark/RubymineProjects/photo-renamer/images/no_renderer.jpg'))
+  title_field('current', proc { @current_fn = edit_line })
+  @stk = stack(width: 500) do
+    image('/Users/mark/RubymineProjects/photo-renamer/images/no_renderer.jpg')
+  end
 
 
 end
